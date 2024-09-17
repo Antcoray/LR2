08123d6abbe8c626677cba17f6657a780f042103
anton <antonio.kolesnikow@gmail.com>
2024-09-17 20:46:23 +0300

    Task_1

---
+++ Task_1/Task_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+    int main() {
+
+        float x;
+        std::cout << " Введите значение x: " << std::endl;
+        std::cin >> x;
+
+        float x2 = x * x;
+        float c23 = 23 * x2;
+        float c69_8= 69 * x2 +8;
+        float c32_23 = x * (32 + c23);
+
+        float problem1 = c32_23 + c69_8;
+        float problem2 = c69_8 - c32_23;
+
+        std::cout << "23x^3+69x^2+32x+8= " << problem1 << std::endl;
+        std::cout << "-23x^3+69x^2-32x+8= " << problem2 << std::endl;
+
+        return 0;
+
+    }
