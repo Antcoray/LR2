bea19bb843469d91a07b7a0810cf1f0f839b8daf
anton <antonio.kolesnikow@gmail.com>
2024-09-22 00:17:44 +0300

    Task_7 done

---
+++ Task_7/Task_7.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double a = 0, b = 0, c = 0, N = 0, Y = 0;
+
+    std::cout << "Введите a = -13.8: " << std::endl;
+    std::cin >> a;
+    std::cout << "Введите b = 8.9: " << std::endl;
+    std::cin >> b;
+    std::cout << "Введите c = 25: " << std::endl;
+    std::cin >> c;
+    std::cout << "Введите N (2; 56; 7; 3): " << std::endl;
+    std::cin >> N;
+
+    switch((int)N)
+    {
+        case 2: Y = b * c - pow(a,2);
+            break;
+        case 56: Y = b * c;
+            break;
+        case 7: Y = b * c;
+            break;
+        case 3: Y = b * c;
+            break;
+        default: Y = pow((a + b),3);
+
+    }
+
+    std::cout << "Y = "<< Y << std::endl;
+
+    return 0;
+
+}
