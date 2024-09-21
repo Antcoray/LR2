3f420dcb9517bb1b120ae77a746c635f727f18ab
anton <antonio.kolesnikow@gmail.com>
2024-09-21 14:31:08 +0300

    variant 2

---
+++ Task_6/Task_6.cpp
@@ -0,0 +1,26 @@
+// Y=1+(K^2)/(2AB)-B+DC, A=x+sin(p), B=e^K
+// x=7 K=8 p=5 D=2 C=6
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double Y = 0, K = 0, A = 0, B = 0, D = 0, C = 0, x = 0, p = 0;
+
+    std::cout << "Введите x: " << std::endl;
+    std::cin >> x;
+    std::cout << "Введите K: " << std::endl;
+    std::cin >> K;
+    std::cout << "Введите p: " << std::endl;
+    std::cin >> p;
+    std::cout << "Введите D: " << std::endl;
+    std::cin >> D;
+    std::cout << "Введите C: " << std::endl;
+    std::cin >> C;
+    B = exp(K);
+    A = x + sin(p);
+    Y=1+((pow(K,2))/(2*A*B))-B+D*C;
+
+    std::cout << "Y= " << Y << std::endl;
+
+
+}
