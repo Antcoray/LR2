dbadb270f4844de8bd7efa98ee2c0b46d9d85487
anton <antonio.kolesnikow@gmail.com>
2024-09-22 17:25:17 +0300

    Task_11 done

---
+++ Task_11/Task_11.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int h1 = 0, min1 = 0, h2 = 0, min2 = 0, duration = 0, totalHours = 0, totalMinutes = 0, startTimeMins = 0, endTimeMins = 0;
+
+    std::cout << "Введите время начала решения задач (часы, минуты): " << std::endl;
+    std::cin >> h1 >> min1;
+
+    std::cout << "Введите время окончания решения задач (часы, минуты): " << std::endl;
+    std::cin >> h2 >> min2;
+
+    startTimeMins = h1 * 60 + min1;
+    endTimeMins = h2 * 60 + min2;
+
+    if (endTimeMins >= startTimeMins) {
+        duration = endTimeMins - startTimeMins;
+    } else {
+        duration = (24*60 - startTimeMins) + endTimeMins;
+    }
+
+    totalHours = duration / 60;
+    totalMinutes = duration % 60;
+
+    std::cout << "Студент решал задачи " << totalHours << " часов и " << totalMinutes << " минут " << std::endl;
+
+    return 0;
+}
