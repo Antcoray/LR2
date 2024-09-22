20d1e96df22743552879089e67183cfd2c6130fb
anton <antonio.kolesnikow@gmail.com>
2024-09-22 22:45:12 +0300

    12.1 edited

--- Task_12/Task_12.cpp
+++ Task_12/Task_12.cpp
@@ -1,32 +1,25 @@
-// 1. ax^4 + bx^2 + c = 0;
 #include <iostream>
 #include <cmath>
-
-using namespace std;
+#include <complex>
 
 int main() {
-    double a = 0, b = 0, c = 0, D = 0, y1 = 0, y2 = 0;
+    double a = 0, b = 0, c = 0, D = 0;
     std::cout << "Введите коэффициенты a, b, c для уравнения ax^4 + bx^2 + c = 0: " << std::endl;
-    cin >> a >> b >> c;
+    std::cin >> a >> b >> c;
 
     D = b * b - 4 * a * c;
 
-    if (D < 0) {
-        std::cout << "Нет действительных корней." << std::endl;
-    } else {
-        y1 = (-b + sqrt(D)) / (2 * a);
-        y2 = (-b - sqrt(D)) / (2 * a);
+    std::complex<double> y1 = (-b + sqrt(std::complex<double>(D))) / (2 * a);
+    std::complex<double> y2 = (-b - sqrt(std::complex<double>(D))) / (2 * a);
 
-        if (y1 >= 0) {
-            std::cout << "x1 = " << sqrt(y1) << ", x2 = " << -sqrt(y1) << std::endl;
-        }
-        if (y2 >= 0) {
-            std::cout << "x3 = " << sqrt(y2) << ", x4 = " << -sqrt(y2) << std::endl;
-        }
-        if (y1 < 0 && y2 < 0) {
-            std::cout << "Нет действительных корней." << std::endl;
-        }
-    }
+    std::complex<double> x1 = sqrt(y1);
+    std::complex<double> x2 = -sqrt(y1);
+
+    std::complex<double> x3 = sqrt(y2);
+    std::complex<double> x4 = -sqrt(y2);
+
+    std::cout << "x1 = " << x1 << ", x2 = " << x2 << ", x3 = " << x3 << ", x4 = " << x4 << std::endl;
 
     return 0;
 }
+
