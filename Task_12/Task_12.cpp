22a2e3cc526840faee509ad6e67fdead6fae2b95
anton <antonio.kolesnikow@gmail.com>
2024-09-22 22:30:09 +0300

    12.2 done

--- Task_12/Task_12.cpp
+++ Task_12/Task_12.cpp
@@ -1,32 +1,37 @@
-// 1. ax^4 + bx^2 + c = 0;
 #include <iostream>
 #include <cmath>
-
-using namespace std;
+#include <complex>
 
 int main() {
-    double a = 0, b = 0, c = 0, D = 0, y1 = 0, y2 = 0;
-    std::cout << "Введите коэффициенты a, b, c для уравнения ax^4 + bx^2 + c = 0: " << std::endl;
-    cin >> a >> b >> c;
+    double a = 0, b = 0, c = 0;
 
-    D = b * b - 4 * a * c;
+    std::cout << "Введите a, b, c : " << std::endl;
+    std::cin >> a >> b >> c;
 
-    if (D < 0) {
-        std::cout << "Нет действительных корней." << std::endl;
-    } else {
-        y1 = (-b + sqrt(D)) / (2 * a);
-        y2 = (-b - sqrt(D)) / (2 * a);
+    // замена t = x + 1 / x;
 
-        if (y1 >= 0) {
-            std::cout << "x1 = " << sqrt(y1) << ", x2 = " << -sqrt(y1) << std::endl;
-        }
-        if (y2 >= 0) {
-            std::cout << "x3 = " << sqrt(y2) << ", x4 = " << -sqrt(y2) << std::endl;
-        }
-        if (y1 < 0 && y2 < 0) {
-            std::cout << "Нет действительных корней." << std::endl;
-        }
-    }
+    // at^2 + bt + (c - 2a) = 0 --- новое уравнение
+
+    double Dt = b * b - 4 * a * (c - 2 * a);
+
+    // Решаем уравнение для t1 и t2
+
+    std::complex<double> t1 = (-b - sqrt(std::complex<double>(Dt))) / (2 * a);
+    std::complex<double> t2 = (-b + sqrt(std::complex<double>(Dt))) / (2 * a);
+
+    // x^2 - t1x + 1 = 0 и x^2 - t2x + 1 = 0 --- новые уравнения
+
+    std::complex<double> Dx1 = t1 * t1 - std::complex<double>(4.0);
+    std::complex<double> x1 = (-t1 - sqrt(Dx1)) / std::complex<double>(2.0);
+    std::complex<double> x2 = (-t1 + sqrt(Dx1)) / std::complex<double>(2.0);
+
+    std::complex<double> Dx2 = t2 * t2 - std::complex<double>(4.0);
+    std::complex<double> x3 = (-t2 - sqrt(Dx2)) / std::complex<double>(2.0);
+    std::complex<double> x4 = (-t2 + sqrt(Dx2)) / std::complex<double>(2.0);
+
+    std::cout << "x1, x2, x3, x4: " << x1 << ", " << x2 << ", " << x3 << ", " << x4 << std::endl;
 
     return 0;
 }
+
+
