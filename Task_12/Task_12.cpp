9c2484101c914a74fbf0c52eb37477d0cad18422
anton <antonio.kolesnikow@gmail.com>
2024-09-23 17:15:52 +0300

    12.3 in progress

--- Task_12/Task_12.cpp
+++ Task_12/Task_12.cpp
@@ -1,25 +1,30 @@
+// 3. x^3 + px + q = 0 --- каноническая форма кубического уравнения
 #include <iostream>
 #include <cmath>
 #include <complex>
 
+double defF(double q, double p){
+    double F = 0;
+    if (q < 0) {
+        F = atan(sqrt(-(pow(q, 2) / 4 + pow(p, 3) / 27)) / (-q / 2));
+    } else if (q > 0) {
+        F = atan(sqrt(-(pow(q, 2) / 4 + pow(p, 3) / 27)) / (-q / 2) + M_PI);
+    } else {
+        F = M_PI / 2;
+    }
+    return F;
+}
+
 int main() {
-    double a = 0, b = 0, c = 0, D = 0;
-    std::cout << "Введите коэффициенты a, b, c для уравнения ax^4 + bx^2 + c = 0: " << std::endl;
-    std::cin >> a >> b >> c;
+    double p = 0, q = 0;
 
-    D = b * b - 4 * a * c;
+    std::cout << "Введите p, q:" << std::endl;
+    std::cin >> p >> q;
 
-    std::complex<double> y1 = (-b + sqrt(std::complex<double>(D))) / (2 * a);
-    std::complex<double> y2 = (-b - sqrt(std::complex<double>(D))) / (2 * a);
+    double F = defF(q, p);
 
-    std::complex<double> x1 = sqrt(y1);
-    std::complex<double> x2 = -sqrt(y1);
-
-    std::complex<double> x3 = sqrt(y2);
-    std::complex<double> x4 = -sqrt(y2);
-
-    std::cout << "x1 = " << x1 << ", x2 = " << x2 << ", x3 = " << x3 << ", x4 = " << x4 << std::endl;
-
+    std::cout << " x1 = " << F << " x2 = " << F << std::endl;
+     
     return 0;
 }
 
