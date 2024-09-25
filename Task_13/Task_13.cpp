5bd21621d6dd538bb206dcb4336ae67ab6cba1da
anton <antonio.kolesnikow@gmail.com>
2024-09-25 22:35:31 +0300

    LR2 done

--- Task_13/Task_13.cpp
+++ Task_13/Task_13.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 
 int main() {
-int a = 0, b = 0;
+    int a = 0, b = 0;
 
-int *pa;
-int *pb;
+    std::cout << "\nEnter variable a (integer) \n" << std::endl;
+    std::cin >> a;
+    int *pa = &a;
 
-std::cout << "\nEnter variable a (integer) \n" << std::endl;
-std::cin >> a;
-pa = &a;
+    std::cout << "\nVariable a: address = "<< pa <<"\t value = "<< *pa << std::endl;
 
-std::cout << "\nVariable a: address = "<< pa <<"\t value = "<< *pa << std::endl;
+    std::cout << "\nEnter variable b (integer) \n" << std::endl;
+    std::cin >> b;
+    int *pb = &b;
+    std::cout <<"\nVariable b: address = "<< pb <<"\t value = "<< *pb << "\n" << std::endl;
 
-std::cout << "\nEnter variable b (integer) \n" << std::endl;
-std::cin >> b;
-pb = &b;
-std::cout <<"\nVariable b: address = "<< pb <<"\t value = "<< *pb << "\n" << std::endl;
-
-for(int x = 0; x <= 15; x++) {
-pa = &a;
-pb = &b;
-a++;
-b = b*2;
-std::cout << "Variable a: address = "<< pa <<"\t value = "<< *pa <<"\t | \t" << "Variable b: address = "<< pb <<"\t value = "<< *pb << std::endl;
-}
+    for(int x = 0; x <= 15; x++) {
+        pa = &a;
+        pb = &b;
+        a++;
+        b = b*2;
+        std::cout << "Variable a: address = "<< pa <<"\t value = "<< *pa <<"\t | \t" << "Variable b: address = "<< pb <<"\t value = "<< *pb << std::endl;
+    }
 
 }
