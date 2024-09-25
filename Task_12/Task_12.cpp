a8a021fb08b44645e275fa3ae662b1a1e9841347
anton <antonio.kolesnikow@gmail.com>
2024-09-25 12:18:56 +0300

    Task_12 done

--- Task_12/Task_12.cpp
+++ Task_12/Task_12.cpp
@@ -1,18 +1,43 @@
 // 3. x^3 + px + q = 0 --- каноническая форма кубического уравнения
 #include <iostream>
-#include <cmath>
-#include <complex>
 
-double defF(double q, double p){
-    double F = 0;
-    if (q < 0) {
-        F = atan(sqrt(-(pow(q, 2) / 4 + pow(p, 3) / 27)) / (-q / 2));
-    } else if (q > 0) {
-        F = atan(sqrt(-(pow(q, 2) / 4 + pow(p, 3) / 27)) / (-q / 2) + M_PI);
-    } else {
-        F = M_PI / 2;
+double SQRT(double value) {
+    if (value < 0) {
+        return -1;
     }
-    return F;
+    
+    if (value == 0) {
+        return 0;
+    }
+
+    double U = value;
+    double accuracy = 1e-7;
+    double estimate = value / 2.0;
+
+    while (std::abs(estimate - U) > accuracy) {
+        U = estimate;
+        estimate = (U + value / U) / 2.0;
+    }
+
+    return estimate;
+}
+
+double cube(double value) {
+    if (value == 0) {
+        return 0;
+    }
+
+    double U = value;
+    double accuracy = 1e-7;
+    double diff = 1;
+    
+    while (std::abs(diff) > accuracy) {
+        double estimate = (2 * U + value / (U * U)) / 3;
+        diff = U - estimate;
+        U = estimate;
+    }
+
+    return U;
 }
 
 int main() {
@@ -21,10 +46,13 @@ int main() {
     std::cout << "Введите p, q:" << std::endl;
     std::cin >> p >> q;
 
-    double F = defF(q, p);
-
-    std::cout << " x1 = " << F << " x2 = " << F << std::endl;
-     
+    double Q = (p / 3) * (p / 3) * (p / 3) + (q / 2) * (q / 2);
+    if(Q > 0){
+    double x = cube(-q / 2 + SQRT(Q)) + cube(-q / 2 - SQRT(Q));
+    std::cout << "x = " << x << std::endl;
+    } else {
+    std::cout << "Корни уравнения являются комплексными числами" << std::endl;
+    }
     return 0;
 }
 
