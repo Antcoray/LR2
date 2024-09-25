1b30653a3fd9f6618ba688be12dab05c7dd03af0
anton <antonio.kolesnikow@gmail.com>
2024-09-25 09:32:32 +0300

    edited

--- Task_12/Task_12.cpp
+++ Task_12/Task_12.cpp
@@ -21,11 +21,8 @@ double SQRT(double value) {
     return estimate;
 }
 
-
-
-
 int main() {
-    double a = 0, b = 0, c = 0, t1, t2, Dx1, Dx2, Dt, x1, x2, x3, x4, imt1, imt2, realt1, realt2, imx1, imx2, imx3, imx4, realx1, realx2, realx3, realx4;
+    double a = 0, b = 0, c = 0, t1, t2, Dx1, Dx2, Dt, x1, x2, x3, x4;
 
     std::cout << "Введите a, b, c : " << std::endl;
     std::cin >> a >> b >> c;
@@ -40,33 +37,29 @@ int main() {
     // Решаем уравнение для t1 и t2
     t1 = (-b - SQRT(Dt)) / (2 * a);
     t2 = (-b + SQRT(Dt)) / (2 * a);
-
+    } else {
+    std::cout << "Действительных корней нет" << std::endl;
+    return 0;   
+    }
     // x^2 - t1x + 1 = 0 и x^2 - t2x + 1 = 0 --- новые уравнения
 
     Dx1 = t1 * t1 - 4.0;
-        if (Dx1 >= 0) {
-        x1 = (-t1 - SQRT(Dx1)) / 2.0;
-        x2 = (-t1 + SQRT(Dx1)) / 2.0;
-        } else { // x1, x2 --- комплексные числа
-        realx1 = -t1 / 2;
-        realx2 = -t2 / 2;
-        imx1;
-        }
+    if (Dx1 >= 0) {
+    x1 = (-t1 - SQRT(Dx1)) / 2.0;
+    x2 = (-t1 + SQRT(Dx1)) / 2.0;
+    std::cout << "x1, x2: " << x1 << ", " << x2 << ", " << std::endl;
+    } else { // x1, x2 --- комплексные числа
+    std::cout << "x1, x2 --- комплексные числа " << std::endl;
+    }
     Dx2 = t2 * t2 - 4.0;
-        if (Dx2 >= 0) {
-        x3 = (-t2 - SQRT(Dx2)) /  2.0;
-        x4 = (-t2 + SQRT(Dx2)) /  2.0;
-        } else { //x3, x4 --- комплексные числа
-
-
-
-        }
-    std::cout << "x1, x2, x3, x4: " << x1 << ", " << x2 << ", " << x3 << ", " << x4 << std::endl;
-    } else { //t1,t2 --- комплексные числа 
-
-
-
-}
+    if (Dx2 >= 0) {
+    x3 = (-t2 - SQRT(Dx2)) /  2.0;
+    x4 = (-t2 + SQRT(Dx2)) /  2.0;
+    std::cout << "x3, x4: " << x3 << ", " << x4 << std::endl;
+    } else { //x3, x4 --- комплексные числа
+    std::cout << "x3, x4 --- комплексные числа " << std::endl;
+    }
+        
     return 0;
 }
 
