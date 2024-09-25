a9231960212d79f6add530c58b6548d193e150d1
anton <antonio.kolesnikow@gmail.com>
2024-09-25 18:16:04 +0300

    Task_10 deleted cmath

--- Task_10/Task_10.cpp
+++ Task_10/Task_10.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
-#include <cmath>
+
+double SQRT(double value) {
+    if (value < 0) {
+        return -1;
+    }
+    
+    if (value == 0) {
+        return 0;
+    }
+
+    double U = value;
+    double accuracy = 1e-7;
+    double estimate = value / 2.0;
+
+    while (std::abs(estimate - U) > accuracy) {
+        U = estimate;
+        estimate = (U + value / U) / 2.0;
+    }
+
+    return estimate;
+}
 
 int main() {
     double x1 = 0, y1 = 0, r = 0, x2 = 0, y2 = 0, R = 0;
@@ -10,7 +30,7 @@ int main() {
     std::cout << "Введите координаты и радиус второго круга (x2, y2, R): " << std::endl;
     std::cin >> x2 >> y2 >> R;
 
-    double dstBtwCntrs = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    double dstBtwCntrs = SQRT((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
     if (dstBtwCntrs <= R - r) {
         std::cout << "Да, круг M1 попадает в круг M2" << std::endl;
@@ -24,3 +44,5 @@ int main() {
 
     return 0;
 }
+
+
